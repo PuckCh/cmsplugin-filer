f59e5cafeef2d416044120d5983f00f89c2d345b
Olivier Bornet <olivier.bornet@idiap.ch>
2025-10-23 11:04:56 +0200

    Fix: add on_delete parameters where needed

--- cmsplugin_filer_video/models.py
+++ cmsplugin_filer_video/models.py
@@ -51,6 +51,7 @@ class FilerVideo(CMSPlugin):
         to=CMSPlugin,
         related_name='%(app_label)s_%(class)s',
         parent_link=True,
+        on_delete=models.DO_NOTHING,
     )
 
     def __str__(self):
